80b77f9f72f1a84872ecbaef3111629da9f92262
ahmedfekry <ahmedfikry78@gmail.com>
2014-11-21 18:22:15 +0200

    modified

--- HuffmanWindow/src/Tree.java
+++ HuffmanWindow/src/Tree.java
@@ -1,4 +1,14 @@
+package huffmanwindow;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Ahmed Fekry
+ */
 public abstract class Tree {
 
 	private int Freq;
